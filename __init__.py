8e3ddc08c4300854ed0145a00f64ea242e6d965f
cybernaut4 <arkl1te@protonmail.com>
2025-10-19 21:33:53 +0200

    First commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for arkl1te_toolkit."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Arkl1te"""
+__email__ = "arkl1te@protonmail.com"
+__version__ = "0.0.1"
+
+from .src.arkl1te_toolkit.nodes import NODE_CLASS_MAPPINGS
+from .src.arkl1te_toolkit.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for arkl1te_toolkit."""
