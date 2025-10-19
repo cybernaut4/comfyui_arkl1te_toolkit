2dfe0e5a18b42345e0cf8407c1eb88684ad027c3
cybernaut4 <arkl1te@protonmail.com>
2025-10-20 01:57:48 +0200

    First four nodes

--- src/arkl1te_toolkit/nodes.py
+++ src/arkl1te_toolkit/nodes.py
@@ -1,118 +1,144 @@
 from inspect import cleandoc
-class Example:
+from comfy.comfy_types.node_typing import IO
+import os
+
+class PadZeroes:
     """
-    A example node
-
-    Class methods
-    -------------
-    INPUT_TYPES (dict):
-        Tell the main program input parameters of nodes.
-    IS_CHANGED:
-        optional method to control when the node is re executed.
-
-    Attributes
-    ----------
-    RETURN_TYPES (`tuple`):
-        The type of each element in the output tulple.
-    RETURN_NAMES (`tuple`):
-        Optional: The name of each output in the output tulple.
-    FUNCTION (`str`):
-        The name of the entry-point method. For example, if `FUNCTION = "execute"` then it will run Example().execute()
-    OUTPUT_NODE ([`bool`]):
-        If this node is an output node that outputs a result/image from the graph. The SaveImage node is an example.
-        The backend iterates on these output nodes and tries to execute all their parents if their parent graph is properly connected.
-        Assumed to be False if not present.
-    CATEGORY (`str`):
-        The category the node should appear in the UI.
-    execute(s) -> tuple || None:
-        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
-        For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
+    Takes an `int` number and outputs it with padded zeroes as a `string`.
     """
     def __init__(self):
         pass
 
     @classmethod
     def INPUT_TYPES(s):
-        """
-            Return a dictionary which contains config for all input fields.
-            Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
-            Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
-            The type can be a list for selection.
-
-            Returns: `dict`:
-                - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
-                - Value input_fields (`dict`): Contains input fields config:
-                    * Key field_name (`string`): Name of a entry-point method's argument
-                    * Value field_config (`tuple`):
-                        + First value is a string indicate the type of field or a list for selection.
-                        + Secound value is a config for type "INT", "STRING" or "FLOAT".
-        """
         return {
             "required": {
-                "image": ("Image", { "tooltip": "This is an image"}),
                 "int_field": ("INT", {
                     "default": 0,
-                    "min": 0, #Minimum value
-                    "max": 4096, #Maximum value
-                    "step": 64, #Slider's step
                     "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),   
+                "quantity": ("INT", {
+                    "default": 4,
+                    "display": "number"
                 }),
-                "float_field": ("FLOAT", {
-                    "default": 1.0,
-                    "min": 0.0,
-                    "max": 10.0,
-                    "step": 0.01,
-                    "round": 0.001, #The value represeting the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
-                    "display": "number"}),
-                "print_to_screen": (["enable", "disable"],),
-                "string_field": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
-                    "default": "Hello World!"
-                }),
-            },
+            }
         }
 
-    RETURN_TYPES = ("IMAGE",)
-    #RETURN_NAMES = ("image_output_name",)
+    RETURN_TYPES = ("STRING",)
     DESCRIPTION = cleandoc(__doc__)
-    FUNCTION = "test"
+    FUNCTION = "padZeroes"
+    CATEGORY = "Arkl1te's Toolkit/string"
 
-    #OUTPUT_NODE = False
-    #OUTPUT_TOOLTIPS = ("",) # Tooltips for the output node
-
-    CATEGORY = "Example"
-
-    def test(self, image, string_field, int_field, float_field, print_to_screen):
-        if print_to_screen == "enable":
-            print(f"""Your input contains:
-                string_field aka input text: {string_field}
-                int_field: {int_field}
-                float_field: {float_field}
-            """)
-        #do some processing on the image, in this example I just invert it
-        image = 1.0 - image
-        return (image,)
-
+    def padZeroes(self, int_field, quantity=None):
+        quantity = 4 if quantity is None else quantity
+        padded = str(int_field).zfill(quantity)
+        return (padded,)
+    
+class AnythingToString:
     """
-        The node will always be re executed if any of the inputs change but
-        this method can be used to force the node to execute again even when the inputs don't change.
-        You can make this node return a number or a string. This value will be compared to the one returned the last time the node was
-        executed, if it is different the node will be executed again.
-        This method is used in the core repo for the LoadImage node where they return the image hash as a string, if the image hash
-        changes between executions the LoadImage node is executed again.
+    Takes any type of value and outputs it as a `string`.
     """
-    #@classmethod
-    #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
-    #    return ""
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required":
+            {
+                "input": (IO.ANY,)
+            }
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "convertToString"
+    CATEGORY = "Arkl1te's Toolkit/string"
 
 
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
+    def convertToString(self, input):
+        string = str(input,)
+        return (string,)
+        
+class Concatenate:
+    """
+    Concatenate or join strings, with the optionality to add a **prefix** and a **suffix**.
+
+    NOTE: **prefix** uses the final string as reference, __not__ string_b ! 
+    Be mindful if you're using it for *file paths*!
+    """
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required":
+            {
+                "string_a": ("STRING",),
+                "string_b": ("STRING",)
+            },
+            "optional":
+            {
+                "prefix": ("STRING",),
+                "suffix": ("STRING",),
+            }
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "concatenate"
+    CATEGORY = "Arkl1te's Toolkit/string"
+
+
+    def concatenate(self, string_a, string_b, prefix, suffix):
+        concatenated = ""
+        concatenated = prefix + string_a if prefix else string_a
+        concatenated = concatenated + string_b
+        if suffix:
+            concatenated = concatenated + suffix
+        
+        return (concatenated,)
+    
+
+class GetNewFileIndex:
+    """
+    Counts how many files with the **extension** are in the **directory**.
+    """
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "directory": ("STRING", {
+                    "default": "",
+                    "file_select": "directory",
+                }),
+                "extension": ("STRING", {
+                    "default": "mp4"
+                }),
+            }
+        }
+    
+    RETURN_TYPES = ("INT",)
+    DESCRIPTION = cleandoc(__doc__)
+    FUNCTION = "countFilesWithExtension"
+    CATEGORY = "Arkl1te's Toolkit/string"
+
+    def countFilesWithExtension(self, directory, extension):
+        comfyui_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+
+        directory = (comfyui_root + "\\output\\" + directory.replace("/", "\\"))
+        extension = "." + extension.lower()
+
+        return (len([f for f in os.listdir(directory) if f.lower().endswith(extension)]),)
+
+        
+
 NODE_CLASS_MAPPINGS = {
-    "Example": Example
+    "PadZeroes": PadZeroes,
+    "AnythingToString": AnythingToString,
+    "Concatenate": Concatenate,
+    "GetNewFileIndex": GetNewFileIndex,
 }
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Example": "Example Node"
+    "PadZeroes": "Pad Zeroes",
+    "AnythingToString": "Anything To String",
+    "Concatenate": "Concatenate",
+    "GetNewFileIndex": "Get New File Index",
 }
